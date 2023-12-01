51e56e17e24f9abff4ad754a557981be3e28e423
jamieconnelly <jamiebruceconnelly@gmail.com>
2023-12-01 14:35:50 +0200

    More config

--- api/index.py
+++ api/index.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/python2")
+def hello_world2():
+    return {"message": "Hello World, again"}
+
+@app.get("/api/python3")
+def hello_world3():
+    return {"message": "Hello World, again, again"}
